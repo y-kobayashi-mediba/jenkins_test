548585429cfb66515de41fbfad109fe0d90310b7
小林 佑樹 <y-kobayashi@mediba.jp>
2015-09-07 16:56:18 +0900

    マイグレーションとフィクスチャのロードテスト

---
+++ migrations/m150907_072604_create_news.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150907_072604_create_news extends Migration
+{
+    public function up()
+    {
+      $this->createTable('news', [
+            'id' => 'pk',
+            'title' => Schema::TYPE_STRING . ' NOT NULL',
+            'content' => Schema::TYPE_TEXT,
+        ]);
+    }
+
+    public function down()
+    {
+        echo "m150907_072604_create_news cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
